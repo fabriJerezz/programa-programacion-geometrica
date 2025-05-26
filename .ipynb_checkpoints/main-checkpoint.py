45166369e21ea155d0bd470cba99b11dcc741db7
Lautaro Acosta Quintana <acostaquintanalautaro@gmail.com>
2025-05-26 18:00:44 -0300

    Mejoras al ejemplo y correciones en las constantes alfa,beta, gamma y delta, estaban con los valores cruzados

--- .ipynb_checkpoints/main-checkpoint.py
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -1,10 +1,4 @@
 from gpkit import Variable, Model
-import matplotlib
-
-matplotlib.use("TkAgg")
-
-import matplotlib.pyplot as plt
-
 
 def main():
     # Problema en forma estandar:
@@ -13,28 +7,31 @@ def main():
     w = Variable("w", "m", "Ancho")
     d = Variable("d", "m", "Profundo")
 
+    z = h**-1 * w**-1 * d**-1
+
     # TODO: Aca nos tenemos que decidir cuales van a ser los limites inferiores
     # y superiores del ratio de aspecto
-    alpha = Variable("\\alpha", 0.5, "-")
-    beta = Variable("\\beta", 2.0, "-")
-    gamma = Variable("\\delta", 0.5, "-")
-    delta = Variable("\\gamma", 2.0, "-")
+    alpha = Variable("\\alpha", 1.0)
+    beta = Variable("\\beta", 1.0)
+    gamma = Variable("\\gamma", 1.0)
+    delta = Variable("\\delta", 1.0)
 
-    A_pared = Variable("A_pared", 10, "m^2", "Limite superior del area de la pared")
+    A_pared = Variable("A_pared", 10, "m^2",
+                       "Limite superior del area de la pared")
+
     A_piso = Variable("A_piso", 5, "m^2", "Limite superior del area del piso")
 
-    z = (1 / h) * (1 / w) * (1 / d)
     restricciones = [
-        (2 / A_pared) * h * w + (2 / A_pared) * h * d <= 1,
-        (1 / A_piso) * w * d <= 1,
-
-        alpha / h * w <= 1,
-        (1 / beta) * h * w**-1 <= 1,
+        2 * A_pared**-1 * h * w + 2 * A_pared**-1 * h * d <= 1,
+        A_piso**-1 * w * d <= 1,
+        alpha * h**-1 * w <= 1,
+        beta**-1 * h * w**-1 <= 1,
         gamma * w * d**-1 <= 1,
-        (1 / delta) * w**-1 * d <= 1,
+        delta**-1 * w**-1 * d <= 1,
     ]
 
     modelo = Model(z, restricciones)
+    print(modelo)
     solucion = modelo.solve(verbosity=2)
     print(solucion.table())
 
