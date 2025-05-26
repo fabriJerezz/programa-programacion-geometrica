1347efc1edc655855f21f08db36e78a166972edd
Lautaro Acosta Quintana <acostaquintanalautaro@gmail.com>
2025-05-25 23:06:25 -0300

    Correcion del programa, tambien borre la carpeta env porque no hace falta si tenes uv instalado

--- main.py
+++ main.py
@@ -1,36 +1,38 @@
 from gpkit import Variable, Model
-from gpkit.nomials import Monomial, Posynomial, PosynomialInequality
 
-h = Variable("h", "m")
-w = Variable("w", "m")
-d = Variable("d", "m")
-a_pared = 2 * (h * w + h * d) # type: ignore
-assert isinstance(a_pared, Posynomial)
-a_piso = w * d
-assert isinstance(a_piso, Monomial)
 
-alpha = 2
-beta = 2
-gamma = 3
-delta = 3
-assert isinstance(delta, Monomial)
-# alpha = Variable("alpha")
-# beta = Variable("beta")
-# gamma = Variable("gamma")
+def main():
+    # Problema en forma estandar:
 
-objective = h**-1 * w**-1 * d**-1
+    h = Variable("altura", "m")  # Altura
+    w = Variable("ancho", "m")  # Ancho
+    d = Variable("profundo", "m")  # Profundo
 
-constraints = [
-        (2 / a_pared) * h * w + (2 / a_pared) * h * d <= 1, # type: ignore
-        (1 / a_piso) * w * d <= 1, # type: ignore
+    z = h**-1 * w**-1 * d**-1
+
+    # TODO: Aca nos tenemos que decidir cuales van a ser los limites inferiores
+    # y superiores del ratio de aspecto
+    alpha = Variable("alpha", 1.0)
+    beta = Variable("beta", 1.0)
+    gamma = Variable("gamma", 1.0)
+    delta = Variable("delta", 1.0)
+
+    A_pared = Variable("A_pared", 10, "m^2")
+    A_piso = Variable("A_piso", 5, "m^2")
+
+    restricciones = [
+        2 * A_pared**-1 * h * w + 2 * A_pared**-1 * h * d <= 1,
+        A_piso**-1 * w * d <= 1,
         alpha * h**-1 * w <= 1,
-        (1 / beta) * h * w**-1 <= 1,
+        beta**-1 * h * w**-1 <= 1,
         gamma * w * d**-1 <= 1,
-        (1 / delta) * w**-1*d <=1
+        delta**-1 * w**-1 * d <= 1,
     ]
-    
-m = Model(objective, constraints)
 
-sol = m.solve(verbosity=0)
+    modelo = Model(z, restricciones)
+    solucion = modelo.solve(verbosity=2)
+    print(solucion.table())
 
-print(sol.table())
+
+if __name__ == "__main__":
+    main()
